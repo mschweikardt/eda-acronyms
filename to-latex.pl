131d224b351b01dff2dccb6259174c42ade45889
Yannick Uhlmann <augustunderground@protonmail.com>
2022-08-19 17:49:39 +0200

    glossary identifier in lowercase

--- to-latex.pl
+++ to-latex.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
+my $gls;
 my $line;
 my $readme = "./README.md";
 my $acronyms = "./acronyms.tex";
@@ -18,7 +19,8 @@ while ($line=<README>)
 {
     if ($line =~ /-\s(.+)\s\((.+)\)/gm)
     {
+        $gls = lc $2;
         print ACRONYMS "\\acrodef{$2}{$1}\n";
-        print GLOSSARY "\\newglossaryentry{$2}\n{ name={$2}\n, description={}\n, first={$1 ($2)}\n, long={$1}\n}\n";
+        print GLOSSARY "\\newglossaryentry{$gls}\n{ name={$2}\n, description={}\n, first={$1 ($2)}\n, long={$1}\n}\n";
     }
 }
